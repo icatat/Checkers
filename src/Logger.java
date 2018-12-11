d91f2ab89ddb3cfb75d68cf4094dece0e2c9a04a
arunshrestha <arunshrestha@172.18.51.0>
2018-12-11 17:09:58 -0500

    initial GUI work

---
+++ src/Logger.java
@@ -0,0 +1,13 @@
+
+
+/**
+ * An interface for logging events.
+ * 
+ * @author 
+ */
+public interface Logger {
+    /**
+     * Logs a message from a given source.
+     */
+    public void log(String message, Object source);
+}
