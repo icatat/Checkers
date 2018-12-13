12b4d5978b0910cd82e5810c4fca20a7fc39c595
aditijoshi14 <aditijoshi1997@hotmail.com>
2018-12-13 15:36:59 -0500

    Adding Alpha Beta and MiniMax player

--- src/AlphaBetaCheckersPlayer.java
+++ src/AlphaBetaCheckersPlayer.java
@@ -1,37 +1,150 @@
+import java.util.AbstractSet;
 import java.util.Date;
 
 public class AlphaBetaCheckersPlayer extends CheckersPlayer implements Minimax{
+
+    // Based on the experiment result depthLimit is set to 4.
+    private int depthLimit = 4;
+    private static int staticEvaluations = 0;
+    private static int totalSuccessors = 0;
+    private static int exploredSuccessors = 0;
+    private static int totalParents = 0;
+
     public AlphaBetaCheckersPlayer(String name) {
         super(name);
     }
 
     @Override
-    public Piece getMove(State var1, Date var2) {
-        return null;
+    public Piece getMove(State curState, Date var2) {
+        AbstractSet<State> successors = curState.getSuccessors(true);
+
+        State optimalState = null;
+        State.Player currentPlayer = curState.getCurrentPlayer();
+
+        int evaluation = Integer.MAX_VALUE;
+
+        // Minimax with alpha beta pruning
+        for (State state : successors) {
+            int curEval = minValue(state, Integer.MIN_VALUE, Integer.MAX_VALUE, 1);
+
+            if (curEval < evaluation) {
+                evaluation = curEval;
+                optimalState = state;
+            }
+        }
+
+        if (optimalState == null) return null;
+
+        return optimalState.getPreviousMove();
     }
 
     @Override
     public int staticEvaluator(State state) {
-        return 0;
+        staticEvaluations++;
+        return state.getScore(state.getCurrentPlayer());
     }
 
     @Override
     public int getNodesGenerated() {
-        return 0;
+        return exploredSuccessors;
     }
 
     @Override
     public int getStaticEvaluations() {
-        return 0;
+        return staticEvaluations;
     }
 
     @Override
     public double getAveBranchingFactor() {
-        return 0;
+        return (double)totalSuccessors/(double)totalParents;
     }
 
     @Override
     public double getEffectiveBranchingFactor() {
-        return 0;
+        return (double)exploredSuccessors/(double)totalParents;
     }
+
+    /**
+     * Check if the state is terminal, or if the depth limit has been exceeded
+     *
+     * @param state the state to be evaluated
+     * @param depth current depth of the state
+     * @return true if it terminal state, else return false;
+     */
+    private boolean isTerminalState(State state, int depth) {
+
+        if (depthLimit != -1 && depth >= depthLimit) return true;
+
+        if(state.getStatus() != State.GameStatus.PLAYING) return true;
+
+        return false;
+
+    }
+
+    /**
+     * It maximizes the value of the evaluation function.
+     *
+     * @param state the state to be evaluated
+     * @param a value of the best alternative for max
+     * @param b value of the best alternative for min
+     * @param depth current depth of the state
+     * @return the maximum value of the evaluation function
+     */
+    public int maxValue(State state, int a, int b, int depth) {
+        if (isTerminalState(state, depth)) {
+            return staticEvaluator(state);
+        }
+
+        int v = Integer.MIN_VALUE;
+        AbstractSet<State> successors = state.getSuccessors(true);
+        totalSuccessors += successors.size();
+        totalParents++;
+        depth++;
+
+        for (State s : successors) {
+            if ( s == null) continue;
+
+            exploredSuccessors++;
+            v = Math.max(v, (minValue(s,a, b, depth)));
+
+            if (v >= b) return v;
+
+            a = Math.max(v, a);
+        }
+
+        return v;
+
+    }
+
+    /**
+     * It minimizes the value of the evaluation function.
+     *
+     * @param state the state to be evaluated
+     * @param a value of the best alternative for max
+     * @param b value of the best alternative for min
+     * @param depth current depth of the state
+     * @return the minimum value of the evaluation function
+     */
+
+    public int minValue(State state, int a, int b, int depth) {
+        if (isTerminalState(state, depth)) {
+            return staticEvaluator(state);
+        }
+
+        int v = Integer.MAX_VALUE;
+        AbstractSet<State> successors = state.getSuccessors(true);
+        totalSuccessors+= successors.size();
+        totalParents++;
+        depth++;
+        for (State s : successors) {
+            if (s == null) continue;
+            exploredSuccessors++;
+            v = Math.min(v, (maxValue(s, a, b, depth)));
+            if (v <= a) return v;
+            b = Math.min(v, b);
+        }
+        return v;
+
+    }
+
 }
