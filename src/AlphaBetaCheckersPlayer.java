1f68b80c3580baeb6dbfda18ed9c58a9bebbcb3b
aditijoshi14 <aditijoshi1997@hotmail.com>
2018-12-12 19:51:37 -0500

    Added methods

--- src/AlphaBetaCheckersPlayer.java
+++ src/AlphaBetaCheckersPlayer.java
@@ -1,2 +1,37 @@
+import java.util.Date;
+
 public class AlphaBetaCheckersPlayer extends CheckersPlayer implements Minimax{
+    public AlphaBetaCheckersPlayer(String name) {
+        super(name);
+    }
+
+    @Override
+    public Piece getMove(State var1, Date var2) {
+        return null;
+    }
+
+    @Override
+    public int staticEvaluator(State state) {
+        return 0;
+    }
+
+    @Override
+    public int getNodesGenerated() {
+        return 0;
+    }
+
+    @Override
+    public int getStaticEvaluations() {
+        return 0;
+    }
+
+    @Override
+    public double getAveBranchingFactor() {
+        return 0;
+    }
+
+    @Override
+    public double getEffectiveBranchingFactor() {
+        return 0;
+    }
 }
